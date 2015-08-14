009a4112b34c1fd4235792c4420c4037d5d3ec19
EEEden <edendoyle@inventati.org>
2015-08-14 15:07:57 +0100

    Removed reference to celery

--- mteval/__init__.py
+++ mteval/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask.ext.login import LoginManager
-from celery import Celery
 
 app = Flask(__name__, static_url_path="/static")
 app.config.from_pyfile("config.cfg")
@@ -10,20 +9,6 @@ Bootstrap(app)
 #Login manager initialisation
 lm = LoginManager()
 lm.init_app(app)
-
-def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-
-celery = make_celery(app)
 
 @lm.user_loader
 def load_user(userId):
